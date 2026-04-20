45f6f0ecc4801ca441900290bf9bef5509c1ecd0
Valery Shutikhin <shutikhinv@gmail.com>
2026-04-20 20:39:23 +0300

    feat: add filepath arguments and --format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    console.log('Filepath 1:', filepath1)
+    console.log('Filepath 2:', filepath2)
+    console.log('Format:', options.format || 'stylish')
+  })
   .parse()
